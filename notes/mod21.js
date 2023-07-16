b94dbe20dbbbd4ddf9d3eb70171ae749edbecc86
Arman-Kabir <armankabir8@gmail.com>
2023-07-16 10:31:34 +0600

    previous code added

---
+++ notes/mod21.js
@@ -0,0 +1,12 @@
+/**#1
+ * First step to initialize redux - to create a store.
+ * For TS - set : RootState , AppDispatch at store
+ * 
+ * connection with react app- import provider at main.tsx . import store in Provider
+ * 
+ * set up hook.ts - from redux-toolkit docs.
+ * 
+ * ##2
+ * 
+ * 
+ */
